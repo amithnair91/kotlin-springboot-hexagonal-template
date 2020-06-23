b33c0c22a9ee31f2d9b2b9106d141dc2a17b4331
Amith Nair <amithnair91@gmail.com>
2020-06-24 00:10:48 +0400

    fix organization tests

--- src/test/kotlin/com/amithnair/hexagonaltemplate/domain/OrganizationTest.kt
+++ src/test/kotlin/com/amithnair/hexagonaltemplate/domain/OrganizationTest.kt
@@ -1,13 +1,20 @@
 package com.amithnair.hexagonaltemplate.domain
 
 import com.amithnair.hexagonaltemplate.builders.OrganizationBuilder
-import org.junit.jupiter.api.Test
 import io.kotlintest.shouldBe
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class OrganizationTest {
 
     @Test
     fun `fails if organization name is empty`() {
+        val exception = assertThrows<Exception> { Organization(name = "") }
+        exception.message shouldBe "Organization name cannot be empty"
+    }
+
+    @Test
+    fun `creats organization with valid inputs`(){
         val organizationName = "Microsoft"
         Organization(name = organizationName) shouldBe OrganizationBuilder(name = organizationName).build()
     }
