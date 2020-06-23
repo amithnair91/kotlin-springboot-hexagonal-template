5721696361971d734c24a008c6549818c94ccc94
Amith Nair <amithnair91@gmail.com>
2020-06-24 00:13:45 +0400

    add more organization tests

--- src/main/kotlin/com/amithnair/hexagonaltemplate/domain/Organization.kt
+++ src/main/kotlin/com/amithnair/hexagonaltemplate/domain/Organization.kt
@@ -2,8 +2,8 @@ package com.amithnair.hexagonaltemplate.domain
 
 data class Organization private constructor(val name: String) {
     companion object {
-        operator fun invoke(name: String): Organization {
-            require(!name.isNullOrBlank()) { "Organization name cannot be empty" }
+        operator fun invoke(name: String?): Organization {
+            require(!name.isNullOrBlank()) { "Organization name cannot be null or empty" }
             return Organization(name = name)
         }
     }
--- src/test/kotlin/com/amithnair/hexagonaltemplate/domain/OrganizationTest.kt
+++ src/test/kotlin/com/amithnair/hexagonaltemplate/domain/OrganizationTest.kt
@@ -8,15 +8,21 @@ import org.junit.jupiter.api.assertThrows
 class OrganizationTest {
 
     @Test
-    fun `fails if organization name is empty`() {
-        val exception = assertThrows<Exception> { Organization(name = "") }
-        exception.message shouldBe "Organization name cannot be empty"
+    fun `creates organization with valid inputs`(){
+        val organizationName = "Microsoft"
+        Organization(name = organizationName) shouldBe OrganizationBuilder(name = organizationName).build()
     }
 
     @Test
-    fun `creats organization with valid inputs`(){
-        val organizationName = "Microsoft"
-        Organization(name = organizationName) shouldBe OrganizationBuilder(name = organizationName).build()
+    fun `fails if organization name is empty`() {
+        val exception = assertThrows<Exception> { Organization(name = "") }
+        exception.message shouldBe "Organization name cannot be null or empty"
+    }
+
+    @Test
+    fun `fails to create organization if name is null`(){
+        val exception = assertThrows<Exception> { Organization(name = null) }
+        exception.message shouldBe "Organization name cannot be null or empty"
     }
 
 }
